a74ccbfe662583094143b9cac5344da31bfddb62
es-lo-que-es <elhadodechorte@gmail.com>
2025-09-05 10:39:34 +0300

    correct emote scaling

--- src/interface/single_view.c
+++ src/interface/single_view.c
@@ -35,9 +35,9 @@ void draw_single_view(Emote *emote)
    const Rectangle tr = { r.x, r.y + fsize, r.width, fsize };
    draw_centered_text(emote->info.name, tr, fsize, gconfig->fg);
 
-   const Rectangle r0 = get_centered_rec_at(tr.y + fsize + p, r.width, 24);
-   const Rectangle r1 = get_centered_rec_at(r0.y + r0.height + p, r.width, 48);
-   const Rectangle r2 = get_centered_rec_at(r1.y + r1.height + p, r.width, 72);
+   const Rectangle r0 = get_centered_rec_at(tr.y + fsize + p, r.width, 32);
+   const Rectangle r1 = get_centered_rec_at(r0.y + r0.height + p, r.width, 64);
+   const Rectangle r2 = get_centered_rec_at(r1.y + r1.height + p, r.width, 128);
 
    if ( handle_rec_input(r0) ) copy_emote(emote, r0);
    if ( handle_rec_input(r1) ) copy_emote(emote, r1);
